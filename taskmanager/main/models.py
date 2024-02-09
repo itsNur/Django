a659139471cdfec55d97c8dff39a8f8760d2e26e
Nursulatan <adiev_n@mail.ru>
2024-02-09 14:14:51 +0300

    add model Team

--- taskmanager/main/models.py
+++ taskmanager/main/models.py
@@ -23,3 +23,11 @@ class Participant(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
