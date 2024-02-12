6e4fa0d70ed11c384b51844fcd38d01e93af3449
Nursulatan <adiev_n@mail.ru>
2024-02-12 09:54:06 +0300

    deleted task, team models

--- taskmanager/main/models.py
+++ taskmanager/main/models.py
@@ -1,14 +1,6 @@
 from django.db import models
 
 
-class Task(models.Model):
-    title = models.CharField('Название', max_length=50)
-    task = models.TextField('Описание')
-
-    def __str__(self):
-        return self.title
-
-
 class Command(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -20,14 +12,6 @@ class Command(models.Model):
 class Participant(models.Model):
     name = models.CharField(max_length=100)
     command = models.ForeignKey(Command, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
-
-class Team(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
 
     def __str__(self):
         return self.name
--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
-from .models import Task
 
 
 def index(request):
-    tasks = Task.objects.all()
-    return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'tasks': tasks})
+    return render(request, 'main/index.html')
 
 
 def about(request):
